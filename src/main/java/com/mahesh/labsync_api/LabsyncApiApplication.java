ab5e31da0ab0592c816f074de1f448eba00d1935
mmahesh7 <mmahesh23022@gmail.com>
2026-01-03 12:42:55 +0530

    Project setup

---
+++ src/main/java/com/mahesh/labsync_api/LabsyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.labsync_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/labsync_api/LabsyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.labsync_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
