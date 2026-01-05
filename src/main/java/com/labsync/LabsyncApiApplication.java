52c75a94d14b23c9755bce1584a8d91d23710988
mmahesh7 <mmahesh23022@gmail.com>
2026-01-05 20:22:07 +0530

    Database configured

--- src/main/java/com/labsync/LabsyncApiApplication.java
+++ src/main/java/com/labsync/LabsyncApiApplication.java
@@ -1,4 +1,4 @@
-package com.labsync.labsync_api;
+package com.labsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/labsync/LabsyncApiApplicationTests.java
+++ src/test/java/com/labsync/LabsyncApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.labsync.labsync_api;
+package com.labsync;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
